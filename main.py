075c4240552cea0915990b7fb02c6ff192d15507
Heizenburglar <egorkabumbum01@gmail.com>
2026-05-03 19:05:49 +0300

    ball'n rackets

--- main.py
+++ main.py
@@ -6,9 +6,41 @@ window = display.set_mode((win_w,win_h))
 display.set_caption('Сны при температуре 67℃')
 background = transform.scale(image.load('images.jpg'),(win_w,win_h))
 
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,player_speed,player_w,player_h):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(player_w,player_h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+class Player(GameSprite):
+    def upd_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 300:
+            self.rect.y += self.speed
+    def upd_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 300:
+            self.rect.y += self.speed
+racket1 = Player('67.png',20,20,5,100,200)
+racket2 = Player('69.png',600,20,5,100,200)
+ball = GameSprite('balls.png',150,150,2,60,50)
 game = True
 while game:
     window.blit(background,(0,0))
+    racket1.upd_l()
+    racket2.upd_r()
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
     for e in event.get():
         if e.type == QUIT:
             game = False
