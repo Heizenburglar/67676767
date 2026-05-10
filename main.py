0bcf170dfe1b8eafafe1e6a94054a3dbb70dec34
Heizenburglar <egorkabumbum01@gmail.com>
2026-05-10 18:48:53 +0300

    final

--- main.py
+++ main.py
@@ -32,21 +32,31 @@ class Player(GameSprite):
             self.rect.y += self.speed
 racket1 = Player('67.png',20,20,5,100,200)
 racket2 = Player('69.png',600,20,5,100,200)
-ball = GameSprite('balls.png',150,150,2,110,100)
+ball = GameSprite('balls.png',50,50,2,75,75)
 speed_x = 3
 speed_y = 3
 game = True
 clock = time.Clock()
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render('JEDI LOSE', True,(180,0,0))
+lose2 = font1.render('SITH LOSE', True,(180,0,0))
+finish = False
 while game:
     window.blit(background,(0,0))
-    racket1.upd_l()
-    racket2.upd_r()
-    racket1.reset()
-    racket2.reset()
-    ball.reset()
+
+    if finish != True:
+        window.blit(background, (0, 0))
+        racket1.upd_l()
+        racket1.reset()
+        racket2.upd_r()
+        racket2.reset()
+        ball.reset()
+
     for e in event.get():
         if e.type == QUIT:
             game = False
+
     if game == True:
         ball.rect.x += speed_x
         ball.rect.y += speed_y
@@ -54,5 +64,11 @@ while game:
             speed_y *= -1
         if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2,ball):
             speed_x *=-1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1,(200,200))
+    if ball.rect.x > 700:
+        finish = True
+        window.blit(lose2,(200,200))
     display.update()
     clock.tick(200)
